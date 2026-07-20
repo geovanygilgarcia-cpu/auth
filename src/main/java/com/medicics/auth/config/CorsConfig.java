3e639c0afe2f8df499713e6bd31961335ed364db
geovanygilgarcia-cpu <geovany.gil.garcia@gmail.com>
2026-07-20 12:29:55 -0600

    se habilita cors para front

---
+++ src/main/java/com/medicics/auth/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.medicics.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://expmedic-front.onrender.com")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
